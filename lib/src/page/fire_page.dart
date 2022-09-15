02f2f69a4ebae41fa9582c4d93b677949178c553
Ali Ammar <aliammarali125@gmail.com>
2022-09-15 20:16:06 +0300

    init firebase

---
+++ lib/src/page/fire_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FireStorePage extends StatefulWidget {
+  const FireStorePage({super.key});
+
+  @override
+  State<FireStorePage> createState() => _FireStorePageState();
+}
+
+class _FireStorePageState extends State<FireStorePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          
+        ],
+      ) ,
+    );
+  }
+}
